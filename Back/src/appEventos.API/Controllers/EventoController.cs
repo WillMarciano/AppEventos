45aac3b5d3a169cbd8f9ee12b9cb37d9128533e5
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-07-26 16:56:26 -0300

    Evento Model

--- Back/src/appEventos.API/Controllers/EventoController.cs
+++ Back/src/appEventos.API/Controllers/EventoController.cs
@@ -1,3 +1,4 @@
+using appEventos.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace appEventos.API.Controllers;
@@ -6,6 +7,33 @@ namespace appEventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
-    [HttpGet(Name = "GetWeatherForecast")]
-    public string Get() => "value";
+    public IEnumerable<Evento> _evento = new Evento[]
+    {
+        new Evento()
+        {
+            EventoId = 1,
+            Local = "Online",
+            DataEvento = DateTime.Today.ToString(),
+            Tema = "Angualar e .Net Core",
+            Lote = "1º Lote",
+            QtdPessoas = 200
+        },
+        new Evento
+        {
+            EventoId = 2,
+            Local = "Online",
+            DataEvento = DateTime.Today.AddDays(2).ToString(),
+            Tema = "Angualar e .Net Core 5",
+            Lote = "2º Lote",
+            QtdPessoas = 250
+        }
+    };
+
+    public EventoController() { }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get() => _evento;
+
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetBydId(int id) => _evento.Where(evento => evento.EventoId == id);
 }
