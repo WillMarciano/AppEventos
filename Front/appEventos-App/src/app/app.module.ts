b7f1928d23042ad1eb6742db3e7877ecec269781
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-08-01 14:57:18 -0300

    Inclusão bootstrap

--- Front/appEventos-App/src/app/app.module.ts
+++ Front/appEventos-App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +10,7 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @NgModule({
   declarations: [AppComponent, EventosComponent, PalestrantesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
