ae3cf897ab456abfd9194109cb23a2dd2df45ca3
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-08-08 12:32:14 -0300

    Ajuste Property Binding

--- Front/appEventos-App/src/app/eventos/eventos.component.ts
+++ Front/appEventos-App/src/app/eventos/eventos.component.ts
@@ -4,23 +4,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
+  public eventos: any = [];
+  widthImg: number = 50;
+  marginImg: number = 2;
 
-  public eventos: any;
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
 
-  public getEventos() : void {
+  public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)      
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
     );
   }
-
 }
