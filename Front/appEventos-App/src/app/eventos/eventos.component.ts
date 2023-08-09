953d7453b9e29f96dfc017a0fdaba2c9efc67ce1
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-08-09 11:49:00 -0300

    inclusão Filtros de busca

--- Front/appEventos-App/src/app/eventos/eventos.component.ts
+++ Front/appEventos-App/src/app/eventos/eventos.component.ts
@@ -7,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any = [];
+  public events: any = [];
+  public filteredEvents: any = [];
   widthImg: number = 150;
   marginImg: number = 2;
-  showImg: boolean = false;
-  fiterList: string = '';
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEvents = this.filterList
+      ? this.filterEvents(this.filterList)
+      : this.events;
+  }
+
+  filterEvents(filterFor: string): any {
+    filterFor = filterFor.toLocaleLowerCase();
+    return this.events.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filterFor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterFor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -19,14 +40,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
-      (error) => console.log(error)
-    );
-  }
-
   alterImage() {
     this.showImg = !this.showImg;
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      (response) => {
+        this.events = response;
+        this.filteredEvents = this.events;
+      },
+      (error) => console.log(error)
+    );
   }
 }
