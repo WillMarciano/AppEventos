4f312a8d0207c3f387a611f63a9690efc255c933
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-08-16 10:45:10 -0300

    Inclusão Interface de Persistencia

---
+++ Back/src/appEventos.Persistence/IAppEventosPersistence.cs
@@ -0,0 +1,28 @@
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appEventos.Persistence
+{
+    public interface IAppEventosPersistence
+    {
+        //GERAL
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(T[] entity) where T : class;
+        Task<bool> SaveChangesAsync();
+
+        //EVENTOS
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes);
+
+        //PALESTRANTES
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante> GetPalestranteByIdAsync(int palestranteId, bool includeEventos);
+    }
+}
