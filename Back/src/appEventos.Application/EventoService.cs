15b2dafaac2cf5289fa85bc91620300d2f07d49d
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-09-28 17:45:35 -0300

    Evemt Application p1

---
+++ Back/src/appEventos.Application/EventoService.cs
@@ -0,0 +1,86 @@
+﻿using appEventos.Application.Interfaces;
+using appEventos.Domain.Models;
+using appEventos.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appEventos.Application
+{
+    public class EventoService : IEventoService
+    {
+        private readonly IGeralRepository _geralRepository;
+        private readonly IEventoRepository _eventoRepository;
+
+        public EventoService(IGeralRepository geralRepository, IEventoRepository eventoRepository)
+        {
+            _geralRepository = geralRepository;
+            _eventoRepository = eventoRepository;
+        }
+
+        public async Task<Evento> AddEventos(Evento model)
+        {
+            try
+            {
+                _geralRepository.Add<Evento>(model);
+                if (await _geralRepository.SaveChangesAsync()) 
+                    return await _eventoRepository.GetEventoByIdAsync(model.Id, false);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteEvento(int eventoId)
+        {
+            try
+            {
+                var evento = await _eventoRepository.GetEventoByIdAsync(eventoId);
+                if (evento == null) throw new Exception("Não foi possível encontrar evento para remoção.");
+
+                _geralRepository.Delete<Evento>(evento);
+                return await _geralRepository.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Evento?> UpdateEvento(int eventoId, Evento model)
+        {
+            try
+            {
+                var evento = await _eventoRepository.GetEventoByIdAsync(eventoId);
+                if (evento == null) return null;
+
+                model.Id = evento.Id;
+
+                _geralRepository.Update<Evento>(model);
+                if (await _geralRepository.SaveChangesAsync())
+                    return await _eventoRepository.GetEventoByIdAsync(model.Id, false);
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
--- Back/src/appEventos.Application/Interfaces/IEventoService.cs
+++ Back/src/appEventos.Application/Interfaces/IEventoService.cs
@@ -5,7 +5,7 @@ namespace appEventos.Application.Interfaces
     public interface IEventoService
     {
         Task<Evento> AddEventos(Evento model);
-        Task<Evento> UpdateEvento(int eventoId, Evento model);
+        Task<Evento?> UpdateEvento(int eventoId, Evento model);
         Task<bool> DeleteEvento(int eventoId);
         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
