0a7135935a2481980853b4b2a3f6a853e9156cda
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-10-03 11:10:39 -0300

    Ajuste Dependecy Injection

--- Front/appEventos-App/src/app/services/evento.service.ts
+++ Front/appEventos-App/src/app/services/evento.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable(
+  // {  providedIn: 'root'}
+  )
 export class EventoService {
   baseUrl = 'https://localhost:5001/api/eventos';
   constructor(private http: HttpClient) { }
