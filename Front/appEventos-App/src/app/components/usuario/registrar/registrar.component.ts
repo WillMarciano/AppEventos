0ab112227312f251db65843feb9e64ed1fb328e7
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-11-30 12:38:19 -0300

    Ajuste css registrar, ajuste form registrar

--- Front/appEventos-App/src/app/components/usuario/registrar/registrar.component.ts
+++ Front/appEventos-App/src/app/components/usuario/registrar/registrar.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
-  styleUrls: ['./registrar.component.scss']
+  styleUrls: ['./registrar.component.scss'],
 })
 export class RegistrarComponent {
+  form!: FormGroup;
 
+  get f(): any {
+    return this.form.controls;
+  }
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required]],
+      sobrenome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      usuario: ['', [Validators.required]],
+      senha: ['', [Validators.required]],
+      confirmarSenha: ['', [Validators.required]],
+      termos: ['', [Validators.required]],
+    });
+  }
 }
