622e9c11b8392c38fd0d6c176664444b4401e462
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-12-04 22:52:21 -0300

    Data Annotations

--- Back/src/appEventos.Application/Dtos/EventoDto.cs
+++ Back/src/appEventos.Application/Dtos/EventoDto.cs
@@ -1,4 +1,5 @@
 ﻿using appEventos.Domain.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace appEventos.Application.Dtos
 {
@@ -7,10 +8,15 @@ namespace appEventos.Application.Dtos
         public int Id { get; set; }
         public string? Local { get; set; }
         public string? DataEvento { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
         public string? Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string? ImagemUrl { get; set; }
         public string? Telefone { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "{0} em formato inválido ")]
         public string? Email { get; set; }
         public IEnumerable<LoteDto>? Lotes { get; set; }
         public IEnumerable<RedeSocialDto>? RedesSociais { get; set; }
