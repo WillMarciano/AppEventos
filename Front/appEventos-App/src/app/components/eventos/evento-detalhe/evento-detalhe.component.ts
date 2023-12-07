a4b11dc0002f58438eaff9ae8086cf1bf38dbbbc
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-12-07 17:57:01 -0300

    Carregando Evento

--- Front/appEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/appEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,26 +1,52 @@
-import { Component } from '@angular/core';
+import { EventoService } from '@app/services/evento.service';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Evento } from '@app/models/Evento';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
   styleUrls: ['./evento-detalhe.component.scss'],
 })
-export class EventoDetalheComponent {
+export class EventoDetalheComponent implements OnInit{
+  evento!: Evento;
   form!: FormGroup;
 
-  get f(): any{
+  get f(): any {
     return this.form.controls;
   }
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: ActivatedRoute,
+    private eventoService: EventoService
+  ) {}
+
+  public carregarEvento(): void {
+    const eventoIdParam = this.router.snapshot.paramMap.get('id');
+
+    if (eventoIdParam != null) {
+      this.eventoService.getEventoById(+eventoIdParam).subscribe({
+        next: (evento: Evento) => {
+          this.evento = {...evento};
+          this.form.patchValue(this.evento);
+        },
+        error: (error: any) => {
+          console.error(error);
+        },
+        complete: () => {},
+      });
+    }
+  }
 
   ngOnInit(): void {
     this.validation();
+    this.carregarEvento();
   }
 
   public validation(): void {
@@ -42,11 +68,11 @@ export class EventoDetalheComponent {
     });
   }
 
-  public resetForm():void{
+  public resetForm(): void {
     this.form.reset();
   }
 
-  public cssValidator(campoValidacao: FormControl): any{
-    return {'is-invalid': campoValidacao.errors && campoValidacao.touched}
+  public cssValidator(campoValidacao: FormControl): any {
+    return { 'is-invalid': campoValidacao.errors && campoValidacao.touched };
   }
 }
