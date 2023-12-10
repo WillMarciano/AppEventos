b46cc92f6afd8cfde8d26aaa82f23ae73ec08a8b
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-12-10 09:47:26 -0300

    Inclusão ILoteRepository e LoteRepository

---
+++ Back/src/appEventos.Persistence/Interfaces/ILoteRepository.cs
@@ -0,0 +1,9 @@
+﻿using appEventos.Domain.Models;
+
+namespace appEventos.Repository.Interfaces
+{
+    public interface ILoteRepository
+    {
+        Task<Lote?[]> GetLotesByEventoId(int eventoId);
+    }
+}
---
+++ Back/src/appEventos.Persistence/LoteRepository.cs
@@ -0,0 +1,27 @@
+﻿using appEventos.Domain.Models;
+using appEventos.Repository.Context;
+using appEventos.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace appEventos.Repository
+{
+    public class LoteRepository : ILoteRepository
+    {
+        public readonly AppEventosContext _context;
+
+        public LoteRepository(AppEventosContext context) => _context = context;
+        private IQueryable<Lote> FilterQuery(int eventoId)
+        {
+            IQueryable<Lote> query = _context.Lotes.Include(e => e.Evento).Where(l => l.EventoId == eventoId);
+
+            query = query.OrderBy(e => e.Id);
+            return query.AsNoTracking();
+        }
+
+        public async Task<Lote?[]> GetLotesByEventoId(int eventoId)
+        {
+            var lote = await FilterQuery(eventoId).ToArrayAsync();
+            return lote;
+        }
+    }
+}
