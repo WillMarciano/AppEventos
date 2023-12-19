5897d51033bb34439e2bcecb3cada3ac67a152f0
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-12-19 11:55:07 -0300

    Ajuste Palestrante

--- Back/src/appEventos.Domain/Models/Palestrante.cs
+++ Back/src/appEventos.Domain/Models/Palestrante.cs
@@ -1,14 +1,14 @@
+using AppEventos.Domain.Identity;
+
 namespace AppEventos.Domain.Models
 {
     public class Palestrante
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string MiniCurriculo { get; set; }
-        public string ImagemUrl { get; set; }
-        public string Telefone { get; set; }
-        public string Email { get; set; }
-        public IEnumerable<RedeSocial> ? RedesSociais { get; set; }
-        public IEnumerable<PalestranteEvento> ? PalestrantesEventos { get; set; }
+        public string? MiniCurriculo { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public IEnumerable<RedeSocial>? RedesSociais { get; set; }
+        public IEnumerable<PalestranteEvento>? PalestrantesEventos { get; set; }
     }
 }
