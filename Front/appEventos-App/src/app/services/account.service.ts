f7b7043c39b4986d900f59d7339a7580689cc924
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-12-28 16:10:13 -0300

    Observable Usuario Atual

--- Front/appEventos-App/src/app/services/account.service.ts
+++ Front/appEventos-App/src/app/services/account.service.ts
@@ -2,26 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '@app/models/identity/User';
 import { environment } from '@environments/environment';
-import { Observable, map, take } from 'rxjs';
+import { Observable, ReplaySubject, map, take } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
+  private currentUserSource = new ReplaySubject<User>(1);
+  //Quando usuario sofrer alteração o token da reflesh para a aplicação que a utiliza
+  currentUser$ = this.currentUserSource.asObservable();
+
   baseUrl = environment.apiURL + 'api/Usuario/';
 
   constructor(private http: HttpClient) {}
 
-  public login(model: any) : Observable<void>{
+  public login(model: any): Observable<void> {
     return this.http.post<User>(this.baseUrl + 'login', model).pipe(
       take(1),
       map((response: User) => {
         const user = response;
-        if(user){
-
+        if (user) {
+          this.setCurrentUser(user);
         }
       })
     );
   }
 
+  logout():void{
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+    this.currentUserSource.complete();
+  }
+
+  private setCurrentUser(user: User): void {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
 }
