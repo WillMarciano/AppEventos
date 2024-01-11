db61cf02760d0bdb9aff5e2f66d55a925c7fe5d2
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2024-01-11 16:34:44 -0300

    Rede Sicuak - Classe Peristencia

---
+++ Back/src/AppEventos.Persistence/Interfaces/IRedeSocialRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppEventos.Domain.Models;
+
+namespace AppEventos.Repository.Interfaces
+{
+    public interface IRedeSocialRepository : IGeralRepository
+    {
+        Task<RedeSocial?> GetRedeSocialEventoByIdAsync(int eventoId,int id);
+        Task<RedeSocial?> GetRedeSocialPalestranteByIdAsync(int palestranteId, int id);
+        Task<RedeSocial[]> GetAllByEventoIdAsync(int eventoId);
+        Task<RedeSocial[]> GetAllByPalestranteIdAsync(int palestranteId);
+    }
+}
---
+++ Back/src/AppEventos.Persistence/RedeSocialRepository.cs
@@ -0,0 +1,44 @@
+﻿using AppEventos.Domain.Models;
+using AppEventos.Repository.Context;
+using AppEventos.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppEventos.Repository
+{
+    public class RedeSocialRepository : GeralRepository, IRedeSocialRepository
+    {
+        public new readonly AppEventosContext _context;
+
+        public RedeSocialRepository(AppEventosContext context) : base(context) => _context = context;
+
+        private IQueryable<RedeSocial> FilterQueryPalestrante()
+        {
+            IQueryable<RedeSocial> query = _context.RedesSocials
+                                            .Include(p => p.Palestrante)
+                                            .OrderBy(p => p.Id)
+                                            .AsNoTracking();
+
+            return query;
+        }
+
+        public async Task<RedeSocial[]> GetAllByEventoIdAsync(int eventoId) 
+            => await FilterQueryPalestrante()
+            .Where(rs => rs.EventoId == eventoId)
+            .ToArrayAsync();
+
+        public async Task<RedeSocial[]> GetAllByPalestranteIdAsync(int palestranteId) 
+            => await FilterQueryPalestrante()
+            .Where(rs => rs.PalestranteId == palestranteId)
+            .ToArrayAsync();
+
+        public async Task<RedeSocial?> GetRedeSocialEventoByIdAsync(int eventoId, int id)
+            => await FilterQueryPalestrante()
+            .Where(rs => rs.EventoId == eventoId && rs.Id == id)
+            .FirstOrDefaultAsync();
+
+        public async Task<RedeSocial?> GetRedeSocialPalestranteByIdAsync(int palestranteId, int id) 
+            => await FilterQueryPalestrante()
+            .Where(rs => rs.PalestranteId == palestranteId && rs.Id == id)
+            .FirstOrDefaultAsync();
+    }
+}
